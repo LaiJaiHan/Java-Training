afc4a55cafd4167cc48155ccf9f204b15a2096dc
LaiJaiHan <LaiJaiHan@users.noreply.github.com>
2019-06-16 15:06:54 +0900

    2016.06.16

--- JavaPractice/src/TowerTransmission.java
+++ JavaPractice/src/TowerTransmission.java
@@ -1,26 +1,33 @@
 
 public class TowerTransmission {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		int [] heights = {6,9,5,7,4};
-		int [] returns = {};
+		int [] returns = new int[heights.length];
 		
-		for (int i = heights.length; i>=1; i--) {
+		
+		for (int i = 0; i<returns.length; i++) returns[i] = 0;
+		
+		for (int i = heights.length-1; i>=0; i--) {
 			
-			int tower_num = i;
+			int n = i-1;
 			
-			for(int k = i; k>=1; k--) {
+			for(int k = n; k>0; k--) {
 				
-				if (heights[k-1] > heights[i-1]) {
+				if (heights[k] > heights[i]) {
 					
-					returns[i-1] = k-1;
+					returns[i] = k+1;
+					
+					break;
 				}
 				
 			}
 			
 		}
+		
 		for (int i = 0; i<returns.length; i++) System.out.print(returns[i]);
 
 	}
